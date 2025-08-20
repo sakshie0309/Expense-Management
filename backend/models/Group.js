c9c41748b68f203803a42baec0f8039ac31bbd34
sakshie0309 <151865662+sakshie0309@users.noreply.github.com>
2025-08-20 13:30:56 +0530

    Update Group.js

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,1 +1,14 @@
+// models/Group.js
+const mongoose = require('mongoose');
 
+const groupSchema = new mongoose.Schema({
+  groupName: String,
+  groupDescription: String,
+  members: [String], // Array of emails (strings)
+  currency: String,
+  category: String,
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
